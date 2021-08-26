bb4e3309b07ed8b3ea3168362614cd14fc796486
root <stephenayodeji16@gmail.com>
2021-08-26 09:24:34 -0700

    My Third Commit ALX School Projects

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
-#include<stdio.h>
+#include <stdio.h>
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
