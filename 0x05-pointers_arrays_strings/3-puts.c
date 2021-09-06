a37dc8a7e895952351df6e51a6e23b592d401077
root <stephenayodeji16@gmail.com>
2021-09-06 06:56:55 -0700

    Pointers and Arrays

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -15,21 +15,4 @@ void _puts(char *str)
 		_putchar(str[i]);
 	}
 	_putchar('\n');
-#include "main.h"
-
-/**
- * _puts - print a string to stdout
- * @str: char array string type
- * Description: Can only use _putchar
- */
-
-void _puts(char *str)
-{
-	int i;
-
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		_putchar(str[i]);
-	}
-	_putchar('\n');
 }}
