0dc67cdf169b3521eab1cc73f26c6b1ffab56130
root <stephenayodeji16@gmail.com>
2021-09-23 14:29:56 -0700

    More Malloc free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -28,7 +28,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 			return (NULL);
 
 		return (mem);
-	} 
+	}
 
 	if (new_size == 0 && ptr != NULL)
 	{
@@ -46,4 +46,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 
 	filler = mem;
+
+	for (index = 0; index < old_size && index < new_size; index++)
+		filler[index] = *ptr_copy++;
+
+	free(ptr);
+	return (mem);
 }
